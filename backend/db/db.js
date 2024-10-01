bc695b8eade3b10415a8b361024dcaaf87efa61c
sangam shinde <sangampshinde@gmail.com>
2024-10-01 23:06:34 +0530

    frontend setup

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,15 +1,12 @@
 const mongoose = require('mongoose');
 
-
-
 const database = async () => {
     try {
         await mongoose.connect(process.env.DB);
         console.log('MongoDB database connection established successfully');
         
     } catch (error) {
-        console.log("Database Connection error: " + error);
-        
+        console.log("Database Connection error: " + error); 
     }
 
 }
